f99cc3ea532942a1d79f984c43f3cc2dc38bb86b
SENA <cielodamarisangulo@gmail.com>
2026-07-20 12:28:37 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -203,7 +203,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/622300_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
